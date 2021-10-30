67870be22ab8663b698b16b1bbe616ae9eb1c78a
Casey Spears <spearscase@gmail.com>
2021-10-29 18:08:17 -0600

    dynamicdivide

--- problems/13-dynamic-divide.js
+++ problems/13-dynamic-divide.js
@@ -18,7 +18,9 @@ divideByFive(50); // returns 10
 
 ***********************************************************************/
 
-// your code here
+let dynamicDivide = divisor => {
+  return num => num / divisor;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
