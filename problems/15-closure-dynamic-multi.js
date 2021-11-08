142d2f3da5380dffebb6dbb50ae99f5da7fcc3f3
Casey Spears <spearscase@gmail.com>
2021-11-07 21:33:24 -0700

    closure dynamic multiply

--- problems/15-closure-dynamic-multi.js
+++ problems/15-closure-dynamic-multi.js
@@ -18,7 +18,21 @@ multiplyByFive(5); // returns 25
 
 ***********************************************************************/
 
-// your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+function dynamicMultiply(num) {
+  return (number) => num * number;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
