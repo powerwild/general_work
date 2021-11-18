f62491ac6c15515a700353443967dbb1b416eea2
Casey Spears <spearscase@gmail.com>
2021-11-17 19:37:28 -0700

    3/4 done with bonus problems

--- data-structures-linked-lists-queue-starter/practice/linked-list-queue-practice.js
+++ data-structures-linked-lists-queue-starter/practice/linked-list-queue-practice.js
@@ -10,11 +10,12 @@ class SinglyLinkedNode {
 class SinglyLinkedList {
     constructor(head = null) {
         this.head = head;
+        this.length = 0;
     }
 
     addToTail(val) {
         let newNode = new SinglyLinkedNode(val);
-
+        this.length++;
         if (!this.head) {
             this.head = newNode;
             return this.head;
@@ -30,46 +31,67 @@ class SinglyLinkedList {
     }
 
     listLength() {
-        // Returns the length of the list
-        // Implement in O(n) and in O(1) time complexit
+        return this.length;
+        // let count = 0;
+        // let node = this.head;
+        // while (node) {
+        //     count++;
+        //     node = node.next;
+        // }
+        // return count;
     }
 
     sumOfNodes() {
-        // Returns the sum of the values of all the nodes
-
-        // Write your hypothesis on the time complexity of this method here
+        let total = 0;
+        let node = this.head;
+        while (node) {
+            total += node.value;
+            node = node.next;
+        }
+        return total;
     }
 
     averageValue() {
-        // Returns the average value of all the nodes
-
-        // Write your hypothesis on the time complexity of this method here
+        return this.sumOfNodes() / this.length;
     }
 
     findNthNode(n) {
-        // Returns the node at the nth index from the head
+        let total = 0;
+        let node = this.head;
+        while (node) {
+            if (total === n) return node;
+            total++;
+            node = node.next;
 
-        // Write your hypothesis on the time complexity of this method here
+        }
+        return 'Does not contain that many nodes.'
     }
 
     findMid() {
-        // Returns the middle node
-        // Implement this as a singly linked list then as a doubly linked list
-            // How do the implementation for singly and doubly vary if at all?
-
-        // Write your hypothesis on the time complexity of this method here
+        let total = 0;
+        let node = this.head;
+        if (this.length % 2 === 0) total++;
+        while (node) {
+            if (total === Math.floor(this.length / 2)) {
+                return node;
+            }
+            total++;
+            node = node.next;
+        }
     }
 
     reverse() {
-        // Returns a new reversed version of the linked list
 
-        // Write your hypothesis on the time complexity of this method here
     }
 
     reverseInPlace() {
-        // Reverses the linked list in-place
-
-        // Write your hypothesis on the time complexity of this method here
+        let newTail = this.head;
+        let previous;
+        let curr = this.head;
+        while (curr.next){
+            previous = curr;
+            
+        }
     }
 }
 
@@ -123,7 +145,15 @@ class DoublyLinkedList {
         // Write your hypothesis on the time complexity of this method here
     }
 }
+const list = new SinglyLinkedList();
+list.addToTail(1);
+list.addToTail(2);
+list.addToTail(3);
+list.addToTail(4);
+list.addToTail(5);
+list.addToTail(6);
 
+list.reverseInPlace();
 module.exports = {
     SinglyLinkedNode,
     SinglyLinkedList,
