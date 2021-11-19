f687cdbda4a81deb4420335d93fa0c6ca8ce597c
Casey Spears <spearscase@gmail.com>
2021-11-19 16:46:41 -0700

    finished some of the whiteboarding

--- ind.js
+++ ind.js
@@ -1,15 +1,1 @@
-function intersect(arr1, arr2, arr = []) {
-    if (!arr2.length) return arr;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr2[arr2.length - 1] === arr1[i]) arr.push(arr1[i]);
-    }
-    arr2.pop();
-    return intersect(arr1, arr2, arr);
-}
-// Examples:
-let start = Date.now();
-console.log(
-    intersect(['a', 'b', 'c', 'd'], ['b', 'd', 'e']), // => [ 'b', 'd' ]
-intersect(['a', 'b', 'c'], ['x', 'y', 'z'])) // => []
-let end = Date.now();
-console.log(end - start);
+
