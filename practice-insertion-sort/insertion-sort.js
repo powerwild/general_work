aa899aed8f86b69e4243feca95c88992cd1c847a
Casey Spears <spearscase@gmail.com>
2021-11-30 11:42:24 -0700

    finished insertion sort problem

--- practice-insertion-sort/insertion-sort.js
+++ practice-insertion-sort/insertion-sort.js
@@ -2,30 +2,23 @@
 // Insertion Sort out-of-place
 // Do not modify the original array
 function insertionSort(arr) {
-  let array = arr.slice;
+  let array = arr.slice();
   let sorted = [];
-  for (let i = 0; i < sorted.length; i++) {
+  while (array.length) {
     console.log(sorted.join(","));
-    if (!sorted.length) sorted.push(array.pop());
-    let el = array[array.length - 1];
-    if (el < arr[i] )
-
-
+    let curr = array.pop();
+    if (sorted.length) {
+     for (let i = 0; i < sorted.length; i++) {
+        if (curr < sorted[i]) {
+        sorted.splice(i, 0, curr);
+        break;
+        }
+      }
+    }else {
+      sorted.push(curr);
+    }
   }
-
-    // Pop a value from the array
-
-    // Create a new spot at the end of the array
-
-    // Walk through the sorted array in reverse order
-      // Check if the value to the left is smaller than the new value
-        // If so, you've reached the insertion point so exit the loop
-        // If not shift the value to the right by 1 and continue
-
-    // Insert the unsorted value at the break point
-
-  // Return the sorted array
-
+  return sorted;
 }
 
 // In-place Insertion Sort
