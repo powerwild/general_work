ea16f13dfab72dc5176b2e1215b3af96be7d0cf8
Casey Spears <spearscase@gmail.com>
2021-12-14 11:11:21 -0700

    finished w9d2 java selectors practice

--- practice-for-week-09-element-selection/selectors.js
+++ practice-for-week-09-element-selection/selectors.js
@@ -3,36 +3,36 @@ const select = () => {
 
     /* Section 1 */
     // 1. Get all seeded fruit elements
-    // Your code here
-    
+    console.log(document.body.querySelectorAll('.seed'))
+
     // 2. Get all seedless fruit elements
-    // Your code here
+    console.log(document.body.querySelectorAll('.seedless'))
 
     // 3. Get first seedless fruit element
-    // Your code here
+    console.log(document.body.querySelector('.seedless'));
 
     /* Section 2 */
     // 4. Get inner span with text "you"
-    // Your code here
+    console.log(document.getElementsByTagName('span')[0]);
 
     // 5. Get all children of element "wrapper"
-    // Your code here
+    console.log(document.getElementById('wrapper').children);
 
     // 6. Get all odd number list items in the list
-    // Your code here
+    console.log(document.getElementsByClassName('odd'));
 
     // 7. Get all even number list items in the list
-    // Your code here
+    console.log(document.body.querySelectorAll('ol :not(.odd)'));
 
     /* Section 3 */
     // 8. Get all tech companies without a class name
-    // Your code here
+    console.log(document.getElementById('three').children[1].querySelector(':not(a[class])').innerText);
 
     // 9. Get "Amazon" list element
-    // Your code here
+    console.log(document.getElementsByClassName('shopping')[0]);
 
     // 10. Get all unicorn list elements (not the image element)
-    // Your code here
+    console.log(document.getElementsByClassName('unicorn'));
 }
 
 window.onload = select;
