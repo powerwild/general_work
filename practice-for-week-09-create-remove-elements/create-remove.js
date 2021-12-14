07c8cb93016068e627a257480d5b28b0f1cc730d
Casey Spears <spearscase@gmail.com>
2021-12-14 12:07:09 -0700

    creating and removing elements with javascript

--- practice-for-week-09-create-remove-elements/create-remove.js
+++ practice-for-week-09-create-remove-elements/create-remove.js
@@ -7,16 +7,9 @@ add.addEventListener("click", async () => {
 
         const url = data.message; // URL of new dog image
 
-        /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
-
-        /*------------ Create new dog card with the url above ------------- */
-        /* (use the HTML structure for the current dog image in the index.html
-            file to create a new image with the url) */
-        // Your code here
-
-        /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here
+       const ul = document.body.querySelector('ul');
+       const breed = url.split('/')[4];
+       ul.innerHTML += `<li><figure><img src=${url}><figcaption>${breed}</figcaption>`
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
@@ -27,18 +20,15 @@ add.addEventListener("click", async () => {
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
-    // Your code here
+    document.getElementsByTagName('li')[0].remove();
 
-    /*-------------------- Remove the first dog card --------------------- */
-    // Your code here
+
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
-    // Your code here
-
-    /*-------------------- Remove the last dog card ----------------------- */
-    // Your code here
+    const arr = document.getElementsByTagName('li');
+    arr[arr.length - 1].remove();
 });
