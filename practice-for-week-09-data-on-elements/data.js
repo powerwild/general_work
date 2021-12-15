05b21b20302b15c7378a7df07cf91e19b7705b9d
Casey Spears <spearscase@gmail.com>
2021-12-15 13:33:40 -0700

    finished the data-type storage practice

--- practice-for-week-09-data-on-elements/data.js
+++ practice-for-week-09-data-on-elements/data.js
@@ -1,1 +1,17 @@
-// Your code here
+window.addEventListener('DOMContentLoaded', e => {
+    const shoppingList = document.getElementById('shopping-list');
+    document.getElementById('add').addEventListener('click', e => {
+        e.preventDefault();
+        const name = document.getElementById('name');
+        const type = document.getElementById('type');
+        const li = document.createElement('li');
+        li.innerText = name.ariaValueMax;
+        li.dataset.type = type.ariaValueMax;
+        shoppingList.append(li);
+        if (li.dataset.type === 'drinks') {
+            li.style.backgroundColor = 'red';
+        }
+        name.value = '';
+        type.value = 'drinks';
+    })
+})
