b0de19e7fe10cb26444e433c0266ba5dc51b6e0c
Casey Spears <spearscase@gmail.com>
2022-01-06 17:00:14 -0700

    step 2 done

--- w11d4_data_driven_apps/routes.js
+++ w11d4_data_driven_apps/routes.js
@@ -1,9 +1,59 @@
 const express = require('express');
+const db = require('./db/models');
+const csrf = require('csurf');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index', {title: 'Home'})
-});
+const csrfProtection = csrf({cookie: true});
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler(async (req, res, next) => {
+    try {
+      const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+      res.render('book-list', { title: 'Books', books });
+    } catch (err) {
+      next(err);
+    }
+  }));
+
+  router.route('/book/add')
+  .get(csrfProtection, (req, res) => {
+    const book = db.Book.build();
+    res.render('book-add', {
+      title: 'Add Book',
+      book,
+      csrfToken: req.csrfToken(),
+    });
+  })
+  .post(csrfProtection, asyncHandler(async (req, res) => {
+    const {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    } = req.body;
+
+    const book = db.Book.build({
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    });
+
+    try {
+      await book.save();
+      res.redirect('/');
+    } catch (err) {
+      res.render('book-add', {
+        title: 'Add Book',
+        book,
+        error: err,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 
 module.exports = router;
