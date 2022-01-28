0173dad44498aefa41840c7cd1f2fb0c90dca40c
Casey Spears <spearscase@gmail.com>
2022-01-28 15:14:01 -0700

    finished the practice assessment

--- assessment-for-week-14-practice-react/src/components/FruitShow.js
+++ assessment-for-week-14-practice-react/src/components/FruitShow.js
@@ -1,12 +1,13 @@
-
+import { useParams } from "react-router-dom";
 
 function FruitShow({fruits}) {
+  const {fruitId} = useParams();
   return (
     <div className='fruit-show'>
-      <h2>{fruits[0].name}</h2>
-      <p>{fruits[0].color}</p>
-      <p>{fruits[0].sweetness}</p>
-      <p>{fruits[0].seeds}</p>
+      <h2>{fruits[fruitId - 1].name}</h2>
+      <p>{fruits[fruitId - 1].color}</p>
+      <p>{fruits[fruitId - 1].sweetness}</p>
+      <p>{fruits[fruitId - 1].seeds}</p>
     </div>
   )
 }
