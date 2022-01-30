a19bd098e8a464ee1694a2dc137c20c7796fce98
Casey Spears <spearscase@gmail.com>
2022-01-30 11:10:44 -0700

    passed first test spec

--- assessment-for-week-14-practice-react/src/components/FruitsIndex.js
+++ assessment-for-week-14-practice-react/src/components/FruitsIndex.js
@@ -1,18 +1,5 @@
-import { Link } from "react-router-dom";
-
-
-function FruitsIndex({fruits}) {
-
-  return (
-
-    <div className='fruits-index'>
-      <h2>Fruits Index</h2>
-      {fruits.map((fruit) => {
-        return <Link to={`/fruits/${fruit.id}`} key={fruit.id} id={fruit.id}>{fruit.name}</Link>
-      })}
-    </div>
-
-  )
+function FruitsIndex() {
+  return null;
 }
 
 export default FruitsIndex;
--- second-w14-assessment/src/App.js
+++ second-w14-assessment/src/App.js
@@ -1,9 +1,11 @@
+import FruitsIndex from "./components/FruitsIndex";
 import fruits from "./mockData/fruits.json";
 
 function App() {
   return (
     <>
       <h1>Welcome to Fruits App</h1>
+      <FruitsIndex fruits={fruits} />
     </>
   );
 }
