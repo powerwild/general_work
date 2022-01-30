b6aa95bed1d8a5804f687ba38467146883420e35
Casey Spears <spearscase@gmail.com>
2022-01-30 11:19:56 -0700

    passed the route specs

--- second-w14-assessment/src/App.js
+++ second-w14-assessment/src/App.js
@@ -1,11 +1,33 @@
+import { Route, Switch } from "react-router-dom";
 import FruitsIndex from "./components/FruitsIndex";
+import FruitForm from "./components/FruitForm";
+import FavoriteFruit from './components/FavoriteFruit';
+import SetFavoriteFruit from "./components/SetFavoriteFruit";
+import FruitShow from "./components/FruitShow";
 import fruits from "./mockData/fruits.json";
 
 function App() {
   return (
     <>
       <h1>Welcome to Fruits App</h1>
-      <FruitsIndex fruits={fruits} />
+      <Switch>
+        <Route exact path='/'>
+          <FruitsIndex fruits={fruits} />
+        </Route>
+        <Route path='/fruits/new'>
+          <FruitForm fruits={fruits} />
+        </Route>
+        <Route path='/fav-fruit'>
+          <FavoriteFruit />
+        </Route>
+        <Route path='/set-fav-fruit'>
+          <SetFavoriteFruit fruits={fruits}/>
+        </Route>
+        <Route path='/fruits/:fruitId'>
+          <FruitShow />
+        </Route>
+        <Route>Page Not Found</Route>
+      </Switch>
     </>
   );
 }
