d1817808c84fac71e43663aabcaed09467cd14ed
Casey Spears <spearscase@gmail.com>
2022-01-30 11:30:29 -0700

    passed the favfruit specs

--- second-w14-assessment/src/components/FavoriteFruit.js
+++ second-w14-assessment/src/components/FavoriteFruit.js
@@ -1,5 +1,15 @@
+import { NavLink } from "react-router-dom";
+import { useFruitContext } from "../context/FavFruitContext";
+
+
 const FavoriteFruit = () => {
-  return null;
+  const { favFruitId } = useFruitContext();
+  return (
+  <div className='fav-fruit'>
+    <h2>Favorite Fruit</h2>
+    <NavLink to={`/fruits/${favFruitId}`} />
+  </div>
+  )
 }
 
 export default FavoriteFruit;
