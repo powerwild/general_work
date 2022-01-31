0d98f5752864d00e67f00e9e0a849cff3736674a
Casey Spears <spearscase@gmail.com>
2022-01-31 10:09:53 -0700

    passed next spec

--- assessment-for-week-14-version-b-react/src/components/Navigation.js
+++ assessment-for-week-14-version-b-react/src/components/Navigation.js
@@ -1,5 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+
 function Navigation() {
-  return null;
+  return (
+    <nav>
+      <NavLink to='/trees'>Trees</NavLink>
+      <NavLink to='/trees/new'>Tree Form</NavLink>
+      <NavLink to='/filtered-trees'>Filtered Trees</NavLink>
+      <NavLink to='/toggle-tree-type'>Toggle Tree Type</NavLink>
+    </nav>
+  );
 }
 
 export default Navigation;
