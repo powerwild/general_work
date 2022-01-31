9b4eec212a2bb85d0c39930b89b6cdb29564ed03
Casey Spears <spearscase@gmail.com>
2022-01-31 10:13:40 -0700

    passed next spec

--- assessment-for-week-14-version-b-react/src/components/TreeShow.js
+++ assessment-for-week-14-version-b-react/src/components/TreeShow.js
@@ -1,5 +1,13 @@
-function TreeShow() {
-  return null;
+
+
+function TreeShow({trees}) {
+  return (
+    <div className='tree-show'>
+      <h2>{trees[0].name}</h2>
+      <p>{trees[0].type}</p>
+      <p>{trees[0].avgHeight}</p>
+    </div>
+  );
 }
 
 export default TreeShow;
