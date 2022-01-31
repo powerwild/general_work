9b34e21ba56ca9b03e2815a62ad65f66ef0d66ab
Casey Spears <spearscase@gmail.com>
2022-01-31 10:02:26 -0700

    passed next spec

--- assessment-for-week-14-version-b-react/src/components/FilteredTreesIndex.js
+++ assessment-for-week-14-version-b-react/src/components/FilteredTreesIndex.js
@@ -1,7 +1,17 @@
 import TreesIndex from './TreesIndex.js';
+import { useTreeContext } from '../context/TreeTypeContext.js';
+import { Link } from 'react-router-dom';
 
-const FilteredTreesIndex = () => {
-  return null;
+const FilteredTreesIndex = ({trees}) => {
+  const { treeTypeId, treeType } = useTreeContext();
+  const typeArray = trees.filter(tree => tree.type === treeType);
+
+  return (
+    <>
+      <h2>{treeType}</h2>
+      <TreesIndex trees={typeArray}/>
+    </>
+  );
 }
 
 export default FilteredTreesIndex;
