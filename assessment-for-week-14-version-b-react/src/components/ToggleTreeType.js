0fe68c29932dfdcd2e1e105a0041d8ed1cc130b4
Casey Spears <spearscase@gmail.com>
2022-01-31 10:07:01 -0700

    passed next spec

--- assessment-for-week-14-version-b-react/src/components/ToggleTreeType.js
+++ assessment-for-week-14-version-b-react/src/components/ToggleTreeType.js
@@ -1,4 +1,8 @@
+import { useTreeContext } from "../context/TreeTypeContext";
+
 const ToggleTreeType = () => {
+  const { treeTypeId, setTreeTypeId, treeType } = useTreeContext();
+
   return (
     <div className="toggle-tree-type">
       <h2>Deciduous or Evergreen?</h2>
@@ -7,6 +11,8 @@ const ToggleTreeType = () => {
           type="radio"
           value="1"
           name="treeTypeId"
+          checked={treeType === 'Deciduous'}
+          onChange={(e) => setTreeTypeId(e.target.value)}
         />
         Deciduous
       </label>
@@ -15,6 +21,8 @@ const ToggleTreeType = () => {
           type="radio"
           value="2"
           name="treeTypeId"
+          checked={treeType === 'Evergreen'}
+          onChange={(e) => setTreeTypeId(e.target.value)}
         />
         Evergreen
       </label>
