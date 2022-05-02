8f7e3c57ab9f337f1e0f2fe464cafe48eed8bb21
Casey Spears <spearscase@gmail.com>
2022-05-02 15:20:03 -0500

    group anagrams

--- foobar/index.js
+++ foobar/index.js
@@ -35,4 +35,44 @@ var addTwoNumbers = function(l1, l2) {
 }
 
 
-const dataJSON = await fetch('')
+// need to refactor for speed
+var isAnagram = function(s, t) {
+    if (!s && !t) return true
+    if (!s || !t) return false;
+    if (s.length !== t.length) return false;
+    first = {};
+    second = {};
+    for(let i = 0; i < s.length; i++) {
+        if (!first[s[i]]) first[s[i]] = 1;
+        else first[s[i]] += 1;
+    }
+    for(let j = 0; j < t.length; j++) {
+        if (!second[t[j]]) second[t[j]] = 1;
+        else second[t[j]] += 1;
+    }
+    let keys = Object.keys(first);
+    for (let k in keys) {
+        let key = keys[k];
+        if (!second[key] || first[key] !== second[key]) return false;
+        else delete second[key];
+    }
+    if (second.length > 0) return false;
+    else return true;
+};
+var groupAnagrams = function(strs) {
+    if (strs.length === 1) return [[strs[0]]];
+    let hash = {};
+    for (let s of strs) hash[s] = [];
+    for (let x of Object.keys(hash)) {
+        for (let y of strs) {
+            if (hash[x] && isAnagram(x, y)) {
+                if (y === '') hash[x].push('');
+                else hash[x].push(y);
+                if (x !== y) delete hash[y];
+            }
+        }
+    }
+    return Object.values(hash).sort((a, b) => a.length - b.length);
+};
+
+
