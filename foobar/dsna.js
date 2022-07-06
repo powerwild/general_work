2f8b20e7b1633945a06a47fc634d9b4148b8dbdd
Casey Spears <spearscase@gmail.com>
2022-07-06 13:12:22 -0600

    working on hacker rank

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1,28 +1,47 @@
-const arr = [];
+// const arr = [];
 
 
-const toHash = (str) => {
-    let index = 0;
-    for (let s of str) {
-        let sCode = s.charCodeAt();
-        index += sCode % 5;
-    }
-    return index;
-}
+// const toHash = (str) => {
+//     let index = 0;
+//     for (let s of str) {
+//         let sCode = s.charCodeAt();
+//         index += sCode % 5;
+//     }
+//     return index;
+// }
 
-const hashInsert = (key, value) => {
-    let i = toHash(key);
-    if (!arr[i]) return arr[i] = value;
-    while (true) {
-        if (!arr[i]) {
-            arr[i] = value;
-            break;
+// const hashInsert = (key, value) => {
+//     let i = toHash(key);
+//     if (!arr[i]) return arr[i] = value;
+//     while (true) {
+//         if (!arr[i]) {
+//             arr[i] = value;
+//             break;
+//         }
+//         else i++;
+//     }
+//     return;
+// };
+
+// hashInsert('arrow', 'yes')
+// hashInsert('worra', 'no')
+// console.log(arr)
+
+
+function goodSegment(badNumbers, lower, upper) {
+    const bad = new Set();
+    for (let num of badNumbers) bad.add(num);
+    console.log(badNumbers)
+    console.log(lower)
+    console.log(upper)
+    let maxSegment = 0;
+    let start = lower;
+    for (let i = lower; i <= upper; i++) {
+        if (bad.has(i) || i === upper) {
+            const diff = i === upper && !bad.has(i) ? (i + 1) - start : i - start;
+            maxSegment = diff > maxSegment ? diff : maxSegment;
+            start = i + 1;
         }
-        else i++;
     }
-    return;
-};
-
-hashInsert('arrow', 'yes')
-hashInsert('worra', 'no')
-console.log(arr)
+    return maxSegment;
+}
