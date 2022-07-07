3f3d0dacb44cd3ec63f23b65c70ee6dc71d47171
Casey Spears <spearscase@gmail.com>
2022-07-07 14:42:26 -0600

    learning user inputs in cli

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -28,20 +28,28 @@
 // console.log(arr)
 
 
-function goodSegment(badNumbers, lower, upper) {
-    const bad = new Set();
-    for (let num of badNumbers) bad.add(num);
-    console.log(badNumbers)
-    console.log(lower)
-    console.log(upper)
-    let maxSegment = 0;
-    let start = lower;
-    for (let i = lower; i <= upper; i++) {
-        if (bad.has(i) || i === upper) {
-            const diff = i === upper && !bad.has(i) ? (i + 1) - start : i - start;
-            maxSegment = diff > maxSegment ? diff : maxSegment;
-            start = i + 1;
-        }
-    }
-    return maxSegment;
-}
+// function goodSegment(badNumbers, lower, upper) {
+//     const bad = new Set();
+//     for (let num of badNumbers) bad.add(num);
+//     console.log(badNumbers)
+//     console.log(lower)
+//     console.log(upper)
+//     let maxSegment = 0;
+//     let start = lower;
+//     for (let i = lower; i <= upper; i++) {
+//         if (bad.has(i) || i === upper) {
+//             const diff = i === upper && !bad.has(i) ? (i + 1) - start : i - start;
+//             maxSegment = diff > maxSegment ? diff : maxSegment;
+//             start = i + 1;
+//         }
+//     }
+//     return maxSegment;
+// }
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+// readline.question(`What's your name? `, name => {
+//     console.log(`Hi ${name}!`);
+//     readline.close();
+// });
