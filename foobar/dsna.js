2fa69af7721e23b7f1102b445e592788b980985e
Casey Spears <spearscase@gmail.com>
2022-09-30 16:08:56 -0500

    75-2 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -8,5 +8,13 @@
 
 
 var pivotIndex = function(nums) {
-
+    let total = 0;
+    for (let i = 0; i < nums.length; ++i) total += nums[i];
+    let lSum = 0;
+    for (let j = 0; j < nums.length; ++j) {
+        total -= nums[j];
+        if (lSum === total) return j;
+        lSum += nums[j];
+    }
+    return -1;
 };
