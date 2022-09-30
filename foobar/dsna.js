5504e102fe82d1cb6de2df094db393abbef69005
Casey Spears <spearscase@gmail.com>
2022-09-30 15:44:49 -0500

    75-2 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1,7 +1,12 @@
-var runningSum = function(nums) {
-    const results = [nums[0]];
-    for (let i = 1; i < nums.length; ++i) {
-        results.push(nums[i] + results[i-1])
-    }
-    return results;
+// var runningSum = function(nums) {
+//     const results = [nums[0]];
+//     for (let i = 1; i < nums.length; ++i) {
+//         results.push(nums[i] + results[i-1])
+//     }
+//     return results;
+// };
+
+
+var pivotIndex = function(nums) {
+
 };
