b213f0dfb3399a6a7e30d295cfb65b3659930e2d
Casey Spears <spearscase@gmail.com>
2022-10-03 15:28:16 -0500

    75-3 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -21,5 +21,21 @@
 
 
 var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
+    const sArr = s.split('');
+    const tArr = t.split('');
+    const sMap = {};
+    const tMap = {};
+    for (let i = 0; i < sArr.length; ++i) {
+        if (!sMap[sArr[i]]) sMap[sArr[i]] = '' + i;
+        else sMap[sArr[i]] += i;
 
+        if (!tMap[tArr[i]]) tMap[tArr[i]] = '' + i;
+        else tMap[tArr[i]] += i;
+    }
+    for (let j = 0; j < sArr.length; ++j) {
+        if (sMap[sArr[j]] !== tMap[tArr[j]]) return false;
+    }
+    return true;
 };
+
