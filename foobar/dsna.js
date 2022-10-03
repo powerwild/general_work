39d94104d4c1fab38463315ee9c4ba772fef8d88
Casey Spears <spearscase@gmail.com>
2022-10-03 15:41:21 -0500

    75-4 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -20,22 +20,39 @@
 // };
 
 
-var isIsomorphic = function(s, t) {
-    if (s.length !== t.length) return false;
+// var isIsomorphic = function(s, t) {
+//     if (s.length !== t.length) return false;
+//     const sArr = s.split('');
+//     const tArr = t.split('');
+//     const sMap = {};
+//     const tMap = {};
+//     for (let i = 0; i < sArr.length; ++i) {
+//         if (!sMap[sArr[i]]) sMap[sArr[i]] = '' + i;
+//         else sMap[sArr[i]] += i;
+
+//         if (!tMap[tArr[i]]) tMap[tArr[i]] = '' + i;
+//         else tMap[tArr[i]] += i;
+//     }
+//     for (let j = 0; j < sArr.length; ++j) {
+//         if (sMap[sArr[j]] !== tMap[tArr[j]]) return false;
+//     }
+//     return true;
+// };
+
+
+var isSubsequence = function(s, t) {
+    if (!s.length) return true;
+    if (s.length && !t.length || s.length > t.length) return false;
     const sArr = s.split('');
     const tArr = t.split('');
-    const sMap = {};
-    const tMap = {};
-    for (let i = 0; i < sArr.length; ++i) {
-        if (!sMap[sArr[i]]) sMap[sArr[i]] = '' + i;
-        else sMap[sArr[i]] += i;
-
-        if (!tMap[tArr[i]]) tMap[tArr[i]] = '' + i;
-        else tMap[tArr[i]] += i;
+    let found = false;
+    let i = 0;
+    for (let j = 0; j < tArr.length; ++j) {
+        const char = sArr[i];
+        if (tArr[j] === char) {
+            ++i;
+            if (i === sArr.length) found = true;
+        }
     }
-    for (let j = 0; j < sArr.length; ++j) {
-        if (sMap[sArr[j]] !== tMap[tArr[j]]) return false;
-    }
-    return true;
+    return found;
 };
-
