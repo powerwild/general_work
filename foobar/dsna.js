914efaadd4958dc58683b61a79fb7c313430822c
Casey Spears <spearscase@gmail.com>
2022-10-07 13:26:24 -0500

    75-15 done;

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -244,6 +244,8 @@
 // };
 
 
-var isValidBST = function(root) {
-
+var isValidBST = function(root, min=-Infinity, max=Infinity) {
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
+    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
 };
