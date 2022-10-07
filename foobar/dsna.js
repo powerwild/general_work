07cce96acc98817de77deea0acc58f043eea19c2
Casey Spears <spearscase@gmail.com>
2022-10-07 13:28:47 -0500

    75-16 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -244,8 +244,13 @@
 // };
 
 
-var isValidBST = function(root, min=-Infinity, max=Infinity) {
-    if (!root) return true;
-    if (root.val <= min || root.val >= max) return false;
-    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+// var isValidBST = function(root, min=-Infinity, max=Infinity) {
+//     if (!root) return true;
+//     if (root.val <= min || root.val >= max) return false;
+//     return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+// };
+
+
+var lowestCommonAncestor = function(root, p, q) {
+    
 };
