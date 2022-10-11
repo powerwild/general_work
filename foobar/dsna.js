6036a6d9ed0d6618ab7a69d88826e86c0f6360c4
Casey Spears <spearscase@gmail.com>
2022-10-11 15:09:35 -0500

    75-22 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -375,6 +375,12 @@
 // };
 
 
-var uniquePaths = function(m, n) {
-
+var uniquePaths = function(m, n, memo={}) {
+    const key = m + ',' + n;
+    if (memo[key]) return memo[key];
+    if (m === 1 || n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+    memo[key] = uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo);
+    return memo[key];
 };
+
