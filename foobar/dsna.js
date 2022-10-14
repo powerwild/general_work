8912c73db1bfc8f92f42ab7aaf0ba916f15c5535
Casey Spears <spearscase@gmail.com>
2022-10-14 16:38:53 -0500

    75-25 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -438,5 +438,10 @@
 
 
 var twoSum = function(nums, target) {
-
+    if (nums.length === 2) return [0, 1];
+    const hash = {};
+    for (let i = 0; i < nums.length; ++i) {
+        if ((target - nums[i]) in hash) return [hash[target - nums[i]], i];
+        hash[nums[i]] = i;
+    }
 };
