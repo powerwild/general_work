649e93deeb3cff2c5ad98f46d3753f7e2829b31a
Casey Spears <spearscase@gmail.com>
2022-10-19 15:14:57 -0500

    finish prob 3 inspirin challenge

--- foobar/challenges.js
+++ foobar/challenges.js
@@ -37,31 +37,71 @@
 // }
 
 
-const commonHosts = [
-    "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com", "google.com", "hotmail.com", "hotmail.co.uk", "live.com","mac.com", "me.com", "mail.com", "msn.com", "outlook.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk"
-]
+// const commonHosts = [
+//     "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com", "google.com", "hotmail.com", "hotmail.co.uk", "live.com","mac.com", "me.com", "mail.com", "msn.com", "outlook.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk"
+// ]
 
-function hostForEmail(email) {
-    var emailArray = email.split("@");
-    return emailArray[emailArray.length - 1];
-}
+// function hostForEmail(email) {
+//     var emailArray = email.split("@");
+//     return emailArray[emailArray.length - 1];
+// }
 
-function isCommonEmailHost(email) {
-    var host = hostForEmail(email);
+// function isCommonEmailHost(email) {
+//     var host = hostForEmail(email);
 
-    /*
-    During the iteration of the commonHosts array the else statement was causing the match to
-    be false for every provider that was not the one being searched for.
-    This was causing the function to return false unless the provider being matched was the last one of the array.
-    I removed the match variable and set the loop to return true as soon as a match is found to reduce the time spent traversing
-    the array otherwise the function will return false if the entire array is traversed without a match being found.
-    */
-    let i = 0;
-    while (i < commonHosts.length) {
-      if (commonHosts[i] === host) {
-        return true;
-      }
-      i++;
+//     /*
+//     During the iteration of the commonHosts array the else statement was causing the match to
+//     be false for every provider that was not the one being searched for.
+//     This was causing the function to return false unless the provider being matched was the last one of the array.
+//     I removed the match variable and set the loop to return true as soon as a match is found to reduce the time spent traversing
+//     the array otherwise the function will return false if the entire array is traversed without a match being found.
+//     */
+//     let i = 0;
+//     while (i < commonHosts.length) {
+//       if (commonHosts[i] === host) {
+//         return true;
+//       }
+//       i++;
+//     }
+//     return false;
+// }
+
+
+function removePunctuation(str) {
+    return str.replace(/[^\w\s-]/g, '')
+  }
+  function isSuffix(str) {
+    const suffix = ['Sr', 'Jr', 'II', 'III', 'IV'];
+    for (let s of suffix) {
+      if (s.toLowerCase() === str.toLowerCase()) return true;
     }
     return false;
-}
+  }
+  function nameToParts(name) {
+    const results = [];
+    const names = name.split(' ');
+    let i = 0;
+    let j = 0;
+    while (j < 5) {
+      let char = '';
+      if (i === 0 || i === 2) {
+        char = removePunctuation(names[i]);
+        ++j;
+      }
+      if (i === 1) {
+        if ((names[i].length < 2 || names[i][1] === '.')) {
+          char = removePunctuation(names[i]);
+          ++j;
+        }
+      }
+      if (i === 3) {
+        if (i < names.length && isSuffix(names[i])) {
+          char = removePunctuation(names[i]);
+        }
+        j = 5;
+      }
+      results.push(char);
+      ++i;
+    }
+    return results;
+  }
