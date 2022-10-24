bbb5ad439c7c92a4272e6fa3b7356c4f675309b3
Casey Spears <spearscase@gmail.com>
2022-10-24 15:27:23 -0500

    hackerrank prob 2 walkthrough

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -529,31 +529,55 @@
 
 
 function scatterPalindrome(strToEvaluate) {
-    const strArr = strToEvaluate[0].split('');
-    const map = {};
-    for (let char of strArr) {
-        map[char] = (map[char] + 1 || 1);
-    }
-    let total = 0;
-    let evens = [];
-    let odds = [];
-    for (let entry of Object.entries(map)) {
-        let [k, v] = entry;
-        if (v % 2 === 0) evens.push([k, v / 2]);
-        else {
-            odds.push([k, 1]);
-            evens.push([k, Math.floor(v / 2)]);
+    const results = [];
+
+    for (let str of strToEvaluate) {
+        let count = 0;
+
+        for (let i = 0; i , str.length; ++i) {
+            let chars = new Set();
+
+            for (let j = i; j < str.length; ++j) {
+                if (chars.has(str[j])) {
+                    chars.delete(str[j]);
+                } else {
+                    chars.add(str[j]);
+                }
+                if (chars.size < 2) ++count;
+            }
+
         }
+
+        results.push(count);
+
     }
-    for (let even of evens) {
-        let [eKey, eVal] = even;
-        for (let odd of odds) {
-            let [oKey, oVal] = odd;
-            if (oKey !== eKey) total += oVal;
-        }
-        total += eVal;
-    }
-    return total;
+    return results;
+
+    // const strArr = strToEvaluate[0].split('');
+    // const map = {};
+    // for (let char of strArr) {
+    //     map[char] = (map[char] + 1 || 1);
+    // }
+    // let total = 0;
+    // let evens = [];
+    // let odds = [];
+    // for (let entry of Object.entries(map)) {
+    //     let [k, v] = entry;
+    //     if (v % 2 === 0) evens.push([k, v / 2]);
+    //     else {
+    //         odds.push([k, 1]);
+    //         evens.push([k, Math.floor(v / 2)]);
+    //     }
+    // }
+    // for (let even of evens) {
+    //     let [eKey, eVal] = even;
+    //     for (let odd of odds) {
+    //         let [oKey, oVal] = odd;
+    //         if (oKey !== eKey) total += oVal;
+    //     }
+    //     total += eVal;
+    // }
+    // return total;
 }
 
 
