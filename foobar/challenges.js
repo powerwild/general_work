136fe66d39db1abeeb232e20cd05e745ee5517ca
Casey Spears <spearscase@gmail.com>
2022-10-31 15:12:46 -0500

    prob 2 start

--- foobar/challenges.js
+++ foobar/challenges.js
@@ -136,21 +136,21 @@
 //     }
 //     return total;
 // }
-function closedPaths(number) {
-    const map = {
-        '0': 1,
-        '4': 1,
-        '6': 1,
-        '9': 1,
-        '8': 2
-    };
-    number = number.toString().split('');
-    let total = 0;
-    for (let i = 0; i < number; ++i) {
-        if (map[number[i]]) total += map[number[i]];
-    }
-    return total;
-}
+// function closedPaths(number) {
+//     const map = {
+//         '0': 1,
+//         '4': 1,
+//         '6': 1,
+//         '9': 1,
+//         '8': 2
+//     };
+//     number = number.toString().split('');
+//     let total = 0;
+//     for (let i = 0; i < number; ++i) {
+//         if (map[number[i]]) total += map[number[i]];
+//     }
+//     return total;
+// }
 
 
 // function createLinkedList(head) {
@@ -178,6 +178,9 @@ function closedPaths(number) {
 //     }
 //     return head;
 // }
+function createLinkedList(head) {
+    
+}
 
 
 // function weightCapacity(weights, maxCapacity) {
