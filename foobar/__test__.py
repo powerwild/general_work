88ffc8b89270a14eb528d96a6cae5125a98d1383
Casey Spears <spearscase@gmail.com>
2022-11-08 13:51:58 -0600

    prob 4 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -315,3 +315,20 @@ from datetime import datetime
 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
+        if not list1: return list2
+        if not list2: return list1
+        new_head = ListNode()
+        curr = new_head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return new_head.next
