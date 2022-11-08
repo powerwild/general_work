b81fdda6ec55e6da5719151659fa7561f9b4e742
Casey Spears <spearscase@gmail.com>
2022-11-08 13:53:45 -0600

    prob 5 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -313,22 +313,24 @@ from datetime import datetime
 #         return s_index >= len(s)
 
 
+# class Solution(object):
+#     def mergeTwoLists(self, list1, list2):
+#         if not list1: return list2
+#         if not list2: return list1
+#         new_head = ListNode()
+#         curr = new_head
+#         while list1 and list2:
+#             if list1.val <= list2.val:
+#                     curr.next = list1
+#                     list1 = list1.next
+#             else:
+#                 curr.next = list2
+#                 list2 = list2.next
+#             curr = curr.next
+#         if list1:
+#             curr.next = list1
+#         if list2:
+#             curr.next = list2
+#         return new_head.next
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        if not list1: return list2
-        if not list2: return list1
-        new_head = ListNode()
-        curr = new_head
-        while list1 and list2:
-            if list1.val <= list2.val:
-                    curr.next = list1
-                    list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
-        if list1:
-            curr.next = list1
-        if list2:
-            curr.next = list2
-        return new_head.next
+    def reverseList(self, head):
