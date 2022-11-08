e2c2156ab07dc2ee4b4949f609297aa501566a67
Casey Spears <spearscase@gmail.com>
2022-11-08 14:40:25 -0600

    prob 9 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -378,14 +378,18 @@ from datetime import datetime
 #         return None
 
 
+# class Solution(object):
+#     def maxProfit(self, prices):
+#         profit = 0
+#         max = prices[-1]
+#         for i in range(len(prices)-2, -1, -1):
+#             if prices[i] > max:
+#                 max = prices[i]
+#             else:
+#                 new_profit = max - prices[i]
+#                 profit = new_profit if new_profit > profit else profit
+#         return profit
+
+
 class Solution(object):
-    def maxProfit(self, prices):
-        profit = 0
-        max = prices[-1]
-        for i in range(len(prices)-2, -1, -1):
-            if prices[i] > max:
-                max = prices[i]
-            else:
-                new_profit = max - prices[i]
-                profit = new_profit if new_profit > profit else profit
-        return profit
+    def longestPalindrome(self, s):
