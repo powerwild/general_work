e9fcc125fa325cd367b0288a7c308aa6e6398b0b
Casey Spears <spearscase@gmail.com>
2022-11-11 14:54:13 -0600

    prob 2 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -555,15 +555,16 @@ from datetime import datetime
 #         return max(nums)
 
 
+# class Solution(object):
+#     def countOdds(self, low, high):
+#         even = 0
+#         odd = 0
+#         if low % 2 == 0:
+#             even += 1
+#         else:
+#             odd += 1
+#         return ((high - low + even) // 2) + odd
+
+
 class Solution(object):
-    def countOdds(self, low, high):
-        even = 0
-        odd = 0
-        if low % 2 == 0:
-            even += 1
-        else:
-            odd += 1
-        return ((high - low + even) // 2) + odd
-        if low % 2 == 0:
-            return (high-low+1)//2
-        return (high-low)//2 + 1
+    def average(self, salary):
