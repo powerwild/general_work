85cfffc96e9cc513dc1a4aa1a172a906a03d2543
Casey Spears <spearscase@gmail.com>
2022-11-18 15:06:49 -0600

    prob 28 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -795,18 +795,22 @@ from datetime import datetime
 #         return '{0}A{1}B'.format(bulls, cows)
 
 
+# class Solution(object):
+#     def backspaceCompare(self, s, t):
+#         new_s = ''
+#         for i in range(len(s)):
+#             if s[i] == '#':
+#                 new_s = new_s[:-1]
+#             else:
+#                 new_s += s[i]
+#         new_t = ''
+#         for i in range(len(t)):
+#             if t[i] == '#':
+#                 new_t = new_t[:-1]
+#             else:
+#                 new_t += t[i]
+#         return new_s == new_t
+
+
 class Solution(object):
-    def backspaceCompare(self, s, t):
-        new_s = ''
-        for i in range(len(s)):
-            if s[i] == '#':
-                new_s = new_s[:-1]
-            else:
-                new_s += s[i]
-        new_t = ''
-        for i in range(len(t)):
-            if t[i] == '#':
-                new_t = new_t[:-1]
-            else:
-                new_t += t[i]
-        return new_s == new_t
+    def decodeString(self, s):
