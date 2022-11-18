b75d385a88f41104af9a4c6bf506079f6a386562
Casey Spears <spearscase@gmail.com>
2022-11-18 15:02:22 -0600

    prob 27 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -797,3 +797,16 @@ from datetime import datetime
 
 class Solution(object):
     def backspaceCompare(self, s, t):
+        new_s = ''
+        for i in range(len(s)):
+            if s[i] == '#':
+                new_s = new_s[:-1]
+            else:
+                new_s += s[i]
+        new_t = ''
+        for i in range(len(t)):
+            if t[i] == '#':
+                new_t = new_t[:-1]
+            else:
+                new_t += t[i]
+        return new_s == new_t
