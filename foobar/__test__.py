7c221809ffc6e9c383933938acabb8e94e9996a7
Casey Spears <spearscase@gmail.com>
2022-11-24 12:38:57 -0600

    prob 14 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -991,3 +991,10 @@
 
 class Solution(object):
     def hasCycle(self, head):
+        visited = set()
+        while head:
+            if head in visited:
+                return True
+            visited.add(head)
+            head = head.next
+        return False
