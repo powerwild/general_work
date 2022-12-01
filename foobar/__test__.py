83f8325dd6729e6e5d6bdead2850af59b4b0c503
Casey Spears <spearscase@gmail.com>
2022-12-01 16:43:49 -0600

    prob 5 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -1159,3 +1159,12 @@
 
 class Solution(object):
     def multiply(self, num1, num2):
+        n1 = 0
+        for n in num1:
+            n1 *= 10
+            n1 += ord(n)-48
+        n2 = 0
+        for n in num2:
+            n2 *= 10
+            n2 += ord(n)-48
+        return str(n1*n2)
