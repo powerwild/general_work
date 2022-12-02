a28be5a284abb1db55d72e65376639dd44b52ec6
Casey Spears <spearscase@gmail.com>
2022-12-02 14:38:04 -0600

    prob 2 start

--- foobar/challenges.js
+++ foobar/challenges.js
@@ -657,3 +657,16 @@
     ORDER BY 1;
 
  */
+
+
+function solution(A, K) {
+    var n = A.length;
+    for (var i = 0; i < n - 1; i++) {
+        if (A[i] + 1 < A[i + 1])
+            return false;
+    }
+    if (A[0] != 1 && A[n - 1] != K)
+        return false;
+    else
+        return true;
+}
