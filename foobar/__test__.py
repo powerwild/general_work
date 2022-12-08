72faad2a519c05f02e692faaa2459e1fb9654cab
Casey Spears <spearscase@gmail.com>
2022-12-08 15:10:56 -0600

    prob 2-6-2 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -1299,3 +1299,12 @@
 
 class Solution(object):
     def isBalanced(self, root):
+        def traverse(node):
+            if not node:
+                return 0
+            left = traverse(node.left)
+            right = traverse(node.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+        return traverse(root) != -1
