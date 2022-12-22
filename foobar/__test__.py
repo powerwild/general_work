10a98f19faca6a10e01b063978bccc2df4480fa6
Casey Spears <spearscase@gmail.com>
2022-12-22 15:41:47 -0600

    prob 1-1-1 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -1680,14 +1680,18 @@
 #         return result
 
 
+# class Solution(object):
+#     def isSameTree(self, p, q):
+#         if not p and not q:
+#             return True
+#         if p and not q:
+#             return False
+#         if not p and q:
+#             return False
+#         if p.val != q.val:
+#             return False
+#         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
 class Solution(object):
-    def isSameTree(self, p, q):
-        if not p and not q:
-            return True
-        if p and not q:
-            return False
-        if not p and q:
-            return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    def containsDuplicate(self, nums):
