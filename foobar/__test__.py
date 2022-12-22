2d3bdb466f099fd2eb30ed20624985a141d660dd
Casey Spears <spearscase@gmail.com>
2022-12-22 16:08:12 -0600

    prob 1-1-2 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -1705,3 +1705,9 @@
 
 class Solution(object):
     def maxSubArray(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        for i in range(len(nums)):
+            if i > 0:
+                nums[i] = max(nums[i-1], nums[i-1] + nums[i], nums[i])
+        return max(nums)
