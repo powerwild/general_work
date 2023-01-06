a1ac11d3d62269177e896190809c24a269e1802e
Casey Spears <spearscase@gmail.com>
2023-01-06 11:20:49 -0600

    ps prob 1-2-1 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -1882,3 +1882,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        nums = '{0:032b}'.format(n)
+        total = 0
+        for num in nums:
+            if num == '1':
+                total += 1
+        return total
