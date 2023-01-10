464de40eed6f8ed4ef438d6fda0cab6ee24b943d
Casey Spears <spearscase@gmail.com>
2023-01-10 14:42:51 -0600

    ps prob 1-4-2 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2024,3 +2024,9 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(1, len(arr)):
+            if i < len(arr) - 1 and arr[i+1] - arr[i] != diff:
+                return False
+        return True
