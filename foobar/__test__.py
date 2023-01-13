7843d8e6739696cde03626dd07e143966508b31f
Casey Spears <spearscase@gmail.com>
2023-01-13 15:30:21 -0600

    ps prob 1-6-3 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2139,10 +2139,14 @@
 #         return total
 
 
+# class Solution(object):
+#     def moveZeroes(self, nums):
+#         index = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[index], nums[i] = nums[i], nums[index]
+#                 index += 1
+
+
 class Solution(object):
-    def moveZeroes(self, nums):
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[index], nums[i] = nums[i], nums[index]
-                index += 1
+    def maximumWealth(self, accounts):
