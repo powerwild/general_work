8b383ea1cbc06531aed19a4c583e42205ba18949
Casey Spears <spearscase@gmail.com>
2023-01-13 15:28:53 -0600

    ps prob 1-6-2 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2141,3 +2141,8 @@
 
 class Solution(object):
     def moveZeroes(self, nums):
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index], nums[i] = nums[i], nums[index]
+                index += 1
