a5d3aded4734146a8b63a9c839ca0a4d65faf7ed
Casey Spears <spearscase@gmail.com>
2023-01-18 14:53:37 -0600

    l75 prob 2-2-1 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2276,3 +2276,11 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        end = len(strs[0])
+        for i in range(1, len(strs)):
+            end = min(end, len(strs[i]))
+            for j in range(min(len(strs[i]), end)):
+                if strs[0][j] != strs[i][j]:
+                    end = j
+                    break
+        return strs[0][:end]
