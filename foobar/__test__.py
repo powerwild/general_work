1cc7b5184c092f8bdddf4f5caefe0bb69044c62c
Casey Spears <spearscase@gmail.com>
2023-01-24 16:29:40 -0600

    prob 66 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2491,4 +2491,13 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-
+        i = len(digits) - 1
+        digits[i] += 1
+        while i >= 0 and digits[i] > 9:
+            digits[i] = 0
+            if i > 0:
+                digits[i-1] += 1
+            else:
+                digits = [1] + digits
+            i -= 1
+        return digits
