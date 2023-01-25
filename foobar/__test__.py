05c75111de5123e7aa3d5abcdd7df15174aff446
Casey Spears <spearscase@gmail.com>
2023-01-25 16:37:08 -0600

    prob 101 done

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2557,3 +2557,14 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        if not root:
+            return True
+        def check(l, r):
+            if not l and not r:
+                return True
+            if not l or not r:
+                return False
+            if l.val != r.val:
+                return False
+            return check(l.left, r.right) and check(l.right, r.left)
+        return check(root, root)
