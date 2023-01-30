6974ed678bbbcad874520aa9291c66c6bd688124
Casey Spears <spearscase@gmail.com>
2023-01-30 12:38:41 -0600

    prob 3 start

--- foobar/challenges.js
+++ foobar/challenges.js
@@ -856,17 +856,22 @@
 // }
 
 
-function plus_one(digits) {
-    let i = digits.length - 1;
-    digits[i] += 1;
-    while (i >= 0 && digits[i] > 9) {
-        digits[i] = 0;
-        if (i > 0) {
-            digits[i-1] += 1;
-        } else {
-            digits.unshift(1);
-        }
-        i -= 1;
-    }
-    return digits;
+// function plus_one(digits) {
+//     let i = digits.length - 1;
+//     digits[i] += 1;
+//     while (i >= 0 && digits[i] > 9) {
+//         digits[i] = 0;
+//         if (i > 0) {
+//             digits[i-1] += 1;
+//         } else {
+//             digits.unshift(1);
+//         }
+//         i -= 1;
+//     }
+//     return digits;
+// }
+
+
+function reverse_parentheses(s) {
+
 }
