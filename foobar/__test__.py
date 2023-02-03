f59594ebca94e3bafc7741f2b5261af1a878afdc
Casey Spears <spearscase@gmail.com>
2023-02-03 16:40:45 -0600

    prob 2-18-2 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2742,22 +2742,30 @@
 #         return res
 
 
+# class Solution(object):
+#     def asteroidCollision(self, asteroids):
+#         """
+#         :type asteroids: List[int]
+#         :rtype: List[int]
+#         """
+#         res = []
+#         for ast in asteroids:
+#             while len(res) and ast < 0 < res[-1]:
+#                 val = ast * -1
+#                 if res[-1] < val:
+#                     res.pop()
+#                     continue
+#                 elif res[-1] == val:
+#                     res.pop()
+#                 break
+#             else:
+#                 res.append(ast)
+#         return res
+
+
 class Solution(object):
-    def asteroidCollision(self, asteroids):
+    def calculate(self, s):
         """
-        :type asteroids: List[int]
-        :rtype: List[int]
+        :type s: str
+        :rtype: int
         """
-        res = []
-        for ast in asteroids:
-            while len(res) and ast < 0 < res[-1]:
-                val = ast * -1
-                if res[-1] < val:
-                    res.pop()
-                    continue
-                elif res[-1] == val:
-                    res.pop()
-                break
-            else:
-                res.append(ast)
-        return res
