86b42c79f4236861457a61ef9820567a70c2f415
Casey Spears <spearscase@gmail.com>
2023-02-06 15:15:36 -0600

    prob 145 start

--- foobar/__test__.py
+++ foobar/__test__.py
@@ -2956,21 +2956,29 @@
 #         return numbers.pop()
 
 
+# class Solution(object):
+#     def preorderTraversal(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: List[int]
+#         """
+#         if not root:
+#             return []
+#         stack = [root]
+#         res = []
+#         while len(stack):
+#             curr = stack.pop()
+#             res.append(curr.val)
+#             if curr.right:
+#                 stack.append(curr.right)
+#             if curr.left:
+#                 stack.append(curr.left)
+#         return res
+
+
 class Solution(object):
-    def preorderTraversal(self, root):
+    def postorderTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
-        if not root:
-            return []
-        stack = [root]
-        res = []
-        while len(stack):
-            curr = stack.pop()
-            res.append(curr.val)
-            if curr.right:
-                stack.append(curr.right)
-            if curr.left:
-                stack.append(curr.left)
-        return res
