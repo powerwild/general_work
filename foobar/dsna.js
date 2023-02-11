8e9530fb7e88e5e6d84329a79655d321dd60cf15
Casey Spears <spearscase@gmail.com>
2023-02-11 10:49:15 -0600

    prob 231 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -831,5 +831,8 @@ function findRemainingBalls(direction, strength) {
 
 
 var isPowerOfTwo = function(n) {
-
+    if (n === 1) return true;
+    let num = 2;
+    while (num < n) num *= 2;
+    return num === n;
 };
