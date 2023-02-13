1d3eba6d43a890c72ccbd2b2874ae4b2e04fbdcf
Casey Spears <spearscase@gmail.com>
2023-02-13 16:17:38 -0600

    prob 263 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -870,5 +870,15 @@ function findRemainingBalls(direction, strength) {
 
 
 var isUgly = function(n) {
-
+    if (n <= 0) return false;
+    function divide(divisor) {
+        while (n % divisor === 0) {
+            n /= divisor;
+        }
+        return;
+    }
+    divide(2);
+    divide(3);
+    divide(5);
+    return n === 1;
 };
