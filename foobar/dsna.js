6dd8ba1edd2f5cada226bf932992665a638a7678
Casey Spears <spearscase@gmail.com>
2023-02-13 16:51:45 -0600

    prob 303 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -911,9 +911,14 @@ function findRemainingBalls(direction, strength) {
 
 
 var NumArray = function(nums) {
+    this.sums = [nums[0]];
+    for (let i = 1; i < nums.length; ++i) {
+        this.sums.push(this.sums[i-1] + nums[i]);
+    }
 
 };
 
 NumArray.prototype.sumRange = function(left, right) {
-
+    if (left === 0) return this.sums[right];
+    else return this.sums[right] - this.sums[left-1];
 };
