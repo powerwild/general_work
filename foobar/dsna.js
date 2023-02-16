953893241fbd0cad97c8d2af36092b7b4a969910
Casey Spears <spearscase@gmail.com>
2023-02-16 12:15:43 -0600

    prob 342 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -924,8 +924,13 @@ function findRemainingBalls(direction, strength) {
 // };
 
 
-var isPowerOfThree = function(n) {
-    if (n <= 0) return false;
-    while (n >= 3) n /= 3;
-    return n === 1;
+// var isPowerOfThree = function(n) {
+//     if (n <= 0) return false;
+//     while (n >= 3) n /= 3;
+//     return n === 1;
+// };
+
+
+var isPowerOfFour = function(n) {
+    
 };
