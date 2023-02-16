161831e9caae31fa5350cddcf6d7e971eff2cee2
Casey Spears <spearscase@gmail.com>
2023-02-16 12:22:45 -0600

    prob 345 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -938,13 +938,18 @@ function findRemainingBalls(direction, strength) {
 // };
 
 
-var reverseString = function(s) {
-    let l = 0;
-    let r = s.length - 1;
-    while (l < r) {
-        [s[l], s[r]] = [s[r], s[l]];
-        ++l;
-        --r;
-    }
-    return;
+// var reverseString = function(s) {
+//     let l = 0;
+//     let r = s.length - 1;
+//     while (l < r) {
+//         [s[l], s[r]] = [s[r], s[l]];
+//         ++l;
+//         --r;
+//     }
+//     return;
+// };
+
+
+var reverseVowels = function(s) {
+
 };
