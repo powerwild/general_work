91a894cfdb274597e8d641080bf731f9ce1311eb
Casey Spears <spearscase@gmail.com>
2023-02-21 14:40:55 -0600

    prob 412 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1045,5 +1045,13 @@ function findRemainingBalls(direction, strength) {
 
 
 var fizzBuzz = function(n) {
-
+    const res = [];
+    for (let i = 1; i <= n; ++i) {
+        let str = '';
+        if (i % 3 === 0) str += 'Fizz';
+        if (i % 5 === 0) str += 'Buzz';
+        if (!str) str += i;
+        res.push(str);
+    }
+    return res;
 };
