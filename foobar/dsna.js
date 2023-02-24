041e0de69ba5a5916cc8166027f177e8ac69f05a
Casey Spears <spearscase@gmail.com>
2023-02-24 14:44:43 -0600

    prob 492 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1235,5 +1235,8 @@ function findRemainingBalls(direction, strength) {
 
 
 var constructRectangle = function(area) {
-    
+    for (let i = Math.floor(Math.sqrt(area)); i >= 1; --i) {
+        if (area % i === 0) return [area/i, i];
+    }
+    return [area, 1];
 };
