c1eb09b85d2d0e57e52179742e13dff2aed5bdc0
Casey Spears <spearscase@gmail.com>
2023-03-02 15:31:35 -0600

    prob 575 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1332,9 +1332,14 @@ function findRemainingBalls(direction, strength) {
 // };
 
 
-var arrayPairSum = function(nums) {
-    nums.sort((a, b) => a - b);
-    let sum = 0;
-    for (let i = 0; i < nums.length; i += 2) sum += nums[i];
-    return sum;
+// var arrayPairSum = function(nums) {
+//     nums.sort((a, b) => a - b);
+//     let sum = 0;
+//     for (let i = 0; i < nums.length; i += 2) sum += nums[i];
+//     return sum;
+// };
+
+
+var distributeCandies = function(candyType) {
+
 };
