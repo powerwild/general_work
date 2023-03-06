abb337f56760d1842d078bed87d325e2e33a4ce5
Casey Spears <spearscase@gmail.com>
2023-03-06 15:49:50 -0600

    prob 3 done

--- foobar/challenges.js
+++ foobar/challenges.js
@@ -1103,5 +1103,12 @@
 
 
 function removeDuplicates(s) {
-
+    const stack = [];
+    for (let i = 0; i < s.length; ++i) {
+        let last = stack.length - 1;
+        if (last >= 0 && stack[last] === s[i]) {
+            stack.pop();
+        } else stack.push(s[i]);
+    }
+    return stack.join('');
 }
