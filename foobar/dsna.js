63fa0a5f929f80c9b069eb5029770a9ffb97aceb
Casey Spears <spearscase@gmail.com>
2023-03-13 15:55:54 -0500

    prob 606 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1540,5 +1540,18 @@ function findRemainingBalls(direction, strength) {
 
 
 var tree2str = function(root) {
-    
+    let str = '' + root.val;
+    function dfs(node) {
+        if (!node) return '';
+        str += '(' + node.val;
+        dfs(node.left);
+        if (node.right && !node.left) str += '()';
+        dfs(node.right);
+        str += ')';
+        return '';
+    }
+    dfs(root.left);
+    if (root.right && !root.left) str += '()';
+    dfs(root.right);
+    return str;
 };
