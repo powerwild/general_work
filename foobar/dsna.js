b2c8c9503801803fd51e62198e0e95179b0a524f
Casey Spears <spearscase@gmail.com>
2023-03-14 13:55:16 -0500

    prob 628 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1557,14 +1557,16 @@ function findRemainingBalls(direction, strength) {
 // };
 
 
-var mergeTrees = function(root1, root2) {
-    function dfs(n1, n2) {
-        if (!n1 && !n2) return null;
-        const left = dfs(n1?.left, n2?.left);
-        const right = dfs(n1?.right, n2?.right);
-        let sum = (n1 ? n1.val : 0) + (n2 ? n2.val : 0);
-        const newN = new TreeNode(sum, left, right);
-        return newN;
-    }
-    return dfs(root1, root2);
-};
+// var mergeTrees = function(root1, root2) {
+//     function dfs(n1, n2) {
+//         if (!n1 && !n2) return null;
+//         const left = dfs(n1?.left, n2?.left);
+//         const right = dfs(n1?.right, n2?.right);
+//         let sum = (n1 ? n1.val : 0) + (n2 ? n2.val : 0);
+//         const newN = new TreeNode(sum, left, right);
+//         return newN;
+//     }
+//     return dfs(root1, root2);
+// };
+
+
