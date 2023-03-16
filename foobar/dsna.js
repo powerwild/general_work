d2412164ea23ec17fd21695173087cd787d9eaae
Casey Spears <spearscase@gmail.com>
2023-03-16 16:26:13 -0500

    prob 706 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1784,17 +1784,17 @@ function findRemainingBalls(direction, strength) {
 
 
 var MyHashMap = function() {
-
+    this.map = {};
 };
 
 MyHashMap.prototype.put = function(key, value) {
-
+    this.map[key] = value;
 };
 
 MyHashMap.prototype.get = function(key) {
-
+    return this.map[key] !== undefined ? this.map[key] : -1;
 };
 
 MyHashMap.prototype.remove = function(key) {
-
+    delete this.map[key];
 };
