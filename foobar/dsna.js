663e62b28c0979724f1e301b0b859f46b3aae6c3
Casey Spears <spearscase@gmail.com>
2023-03-17 16:01:18 -0500

    prob 766 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1868,5 +1868,10 @@ function findRemainingBalls(direction, strength) {
 
 
 var isToeplitzMatrix = function(matrix) {
-
+    for (let i = 1; i < matrix.length; ++i) {
+        for (let j = 1; j < matrix[0].length; ++j) {
+            if (matrix[i][j] !== matrix[i-1][j-1]) return false;
+        }
+    }
+    return true;
 };
