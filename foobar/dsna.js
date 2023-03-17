3d89ba95db04f8f61b8f6df54d242fa92251f6d6
Casey Spears <spearscase@gmail.com>
2023-03-17 14:47:18 -0500

    prob 744 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -1844,5 +1844,8 @@ function findRemainingBalls(direction, strength) {
 
 
 var nextGreatestLetter = function(letters, target) {
-    
+    for (let char of letters) {
+        if (char > target) return char;
+    }
+    return letters[0];
 };
