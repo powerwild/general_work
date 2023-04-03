577fd5d01f307a3dd85a921141ad2be4d50cfa63
Casey Spears <spearscase@gmail.com>
2023-04-03 15:45:20 -0500

    prob 17 done

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -2326,5 +2326,26 @@ function findRemainingBalls(direction, strength) {
 
 
 var letterCombinations = function(digits) {
-    
+    if (!digits) return [];
+    const lets = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    }
+    let res = new Set(['']);
+    for (let dig of digits) {
+        let newRes = new Set();
+        for (let str of res) {
+            for (let char of lets[dig]) {
+                newRes.add(str+char);
+            }
+        }
+        res = newRes;
+    }
+    return [...res];
 };
