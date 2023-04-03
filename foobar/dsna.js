fc329e37626237d2849a584950e881e4485536f4
Casey Spears <spearscase@gmail.com>
2023-04-03 14:24:51 -0500

    prob 8 start

--- foobar/dsna.js
+++ foobar/dsna.js
@@ -2305,12 +2305,17 @@ function findRemainingBalls(direction, strength) {
 // };
 
 
-var validMountainArray = function(arr) {
-    const len = arr.length;
-    if (len < 3) return false;
-    let i = 0;
-    while (i < len-1 && arr[i] < arr[i+1]) ++i;
-    if (i === len-1 || i === 0) return false;
-    while (i < len-1 && arr[i] > arr[i+1]) ++i;
-    return i === len-1;
+// var validMountainArray = function(arr) {
+//     const len = arr.length;
+//     if (len < 3) return false;
+//     let i = 0;
+//     while (i < len-1 && arr[i] < arr[i+1]) ++i;
+//     if (i === len-1 || i === 0) return false;
+//     while (i < len-1 && arr[i] > arr[i+1]) ++i;
+//     return i === len-1;
+// };
+
+
+var myAtoi = function(s) {
+
 };
